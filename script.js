73a3fabab6a2efd54e39b08c62376ea3b29c52f9
Nora <lethithanhngan2706@gmail.com>
2022-08-10 08:36:56 -0400

    Add some UI to the game

--- script.js
+++ script.js
@@ -1,71 +1,103 @@
 
-// 1. create a function called computer play
-// To gte a random number between 2 values inclusive,
-// Math.random() * (max - min + 1) + min to create random numbers from min to max
+// 1. get the references of html elements
+const playerSelection = document.querySelector("#playerChoice");
+const computerSelection = document.querySelector("#computerChoice");
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const resultText = document.querySelector("#roundResult");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+const finalResultText = document.createElement("p");
+let playerPoint=0;
+let computerPoint=0;
+let finalResult;   
+let playerChoice;
+
+
+// add event listener to each button 
+choiceBtns.forEach(button => button.addEventListener('click', () =>
+{
+    
+    if(playerPoint >= 5 || computerPoint >= 5){
+        choiceBtns.forEach(button => button.removeEventListener('click', () => {
+            playerChoice = button.textContent;
+            playRound(playerChoice, computerPlay());
+        }));
+    } else {
+        playerChoice = button.textContent;
+        playRound(playerChoice, computerPlay());
+    }
+}));
+
 
 function computerPlay(){
-    let randomNumber = Math.floor(Math.random()*(3 - 1 + 1)+1) ;
-    let result='';
-    if(randomNumber === 1 ){
-        result = 'rock';
-    } else if (randomNumber === 2){
-        result = 'paper';
-    } else {
-        result = 'scissors';
+    let comp;
+     // number from min to max: (Math.random() * (max - min + 1)) + min
+     let randomNumber = Math.floor(Math.random() * (3 -1 + 1)+1);
+     // if randomNumber === 1, return ROCK
+     switch(randomNumber){
+         case 1: 
+         comp = "ROCK";
+         break;
+         case 2: 
+         comp = "PAPER";
+         break;
+         case 3: 
+         comp = "SCISSORS";
+         break;
+     }
+     return comp;
+}
+
+function playRound(player, computer){
+    
+    let result = checkResult(player, computer);
+    if(result.includes("You win!")){
+        playerPoint++;
+    } else if(result.includes("You lose!")){
+        computerPoint++;
     }
-    return result;
+
+    playerSelection.textContent = `Player: ${player}`;
+    computerSelection.textContent = `Computer: ${computer}`;
+    playerScore.textContent = `Player Score: ${playerPoint}`;
+    computerScore.textContent =  `Computer Score: ${computerPoint}` ;
+    resultText.textContent = `Result: ${result}`;
+    finalResultText.textContent = checkWinner();
+    gameDiv.appendChild(finalResultText);
+    finalResultText.style.color = "blue";
+    finalResultText.style.fontSize = '25px';
 }
 
 
 
-// 2. create a function that play a single round 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    let winningString = '';
-    if(playerSelection === 'rock' && computerSelection === 'paper'){
-        winningString = 'You Lose! Paper beats Rock';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        winningString = 'You Lose! Scissors beats Paper';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        winningString = 'You Lose! Rock beats Scissors';
-    } else if (computerSelection === 'rock' && playerSelection === 'paper'){
-        winningString = 'You Win! Paper beats Rock';
-    } else if (computerSelection === 'paper' && playerSelection === 'scissors'){
-        winningString = 'You Win! Scissors beats Paper';
-    } else if (computerSelection === 'scissors' && playerSelection === 'rock'){
-        winningString = 'You Win! Rock beats Scissors';
-    } else if (playerSelection ===  computerSelection) {
-        winningString = "It's tie! "+ playerSelection + " ties with "+computerSelection;
-    }
-    return winningString;
-} 
-    /* const playerSelection = 'rock';
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection)); */
+function checkResult(playerSelection, computerSelection){
 
+    if(computerSelection === playerSelection){
+        return "Draw!"
+     }
+    else if(computerSelection==="ROCK"){
+       return  (playerSelection==="PAPER")? "You win! "+playerSelection+" beats "+computerSelection : "You lose! "+computerSelection+" beats "+playerSelection;
+     }
+    else if(computerSelection==="PAPER"){
+        return  (playerSelection==="SCISSORS")? "You win! "+playerSelection+" beats "+computerSelection : "You lose! "+computerSelection+" beats "+playerSelection;
+      }
+    else if(computerSelection==="SCISSORS"){
+        return  (playerSelection==="ROCK")? "You win! "+playerSelection+" beats "+computerSelection : "You lose! "+computerSelection+" beats "+playerSelection;
+      }
 
-// 4. Write a game function and report who wins
- function game() {
-    let playerPoint=0;
-    let computerPoint=0;
-    let winner = '';
-    for(let i=0; i <5; i++){
-       let playerSelection = window.prompt('Enter you choice: ');
-       let computerSelection = computerPlay();
-       let winningDeclaration =  playRound(playerSelection, computerSelection);
-       console.log(winningDeclaration);
-        if(winningDeclaration.includes('You Win')){
-            playerPoint++;
-        } else if (winningDeclaration.includes('You Lose')){
-            computerPoint++;
+}
+
+function checkWinner(){
+    let finalResult;
+    if(computerPoint === 5 || playerPoint === 5){
+        if(computerPoint === playerPoint){
+            finalResult = "The game is a Tie!";
+        } else if(computerPoint > playerPoint){
+            finalResult = "You Lost the game to the Computer!";
+        } else {
+            finalResult = "You Win the game";
         }
     }
-    if(playerPoint > computerPoint){
-        winner = 'Game over. You won!';
-    } else if (playerPoint < computerPoint) {
-        winner = 'Game over. You lose!';
-    } else {
-        winner = "Game over. You ties with computer!";
-    }
-    return winner;
- }
+    return finalResult;
+    
+}
